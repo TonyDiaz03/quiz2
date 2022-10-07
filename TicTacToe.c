19ff3eae16e091886f255f942b37e79ca144577e
TonyDiaz03 <112600770+TonyDiaz03@users.noreply.github.com>
2022-10-07 14:30:35 -0700

    Add files via upload

---
+++ TicTacToe.c
@@ -0,0 +1,17 @@
+//Tic Tac Toe game between two players or player vs computer 
+//only need to run the game once
+#include <stdio.h>
+
+int main()
+{
+	//prompt user for game to play (1 or 2)
+	
+	
+	
+	//read information from user on console for where to place piece
+
+	
+	
+	
+	return 0;
+}
